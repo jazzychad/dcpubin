74380bf50db32d49dbc15e7f2f502c46b37898cf
Chad Etzel <chad@jazzychad.net>
2012-04-07 16:45:37 -0700

    fix port for heroku

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.post('/post/new', routes.new_post);
 
 app.get('/posts', routes.post_index);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
